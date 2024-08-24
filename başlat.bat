713737eb563e46b46249194bbc11f0b355ddd1e7
Just <110572436+just1nwashere@users.noreply.github.com>
2024-08-24 22:27:42 +0300

    Add files via upload

---
+++ başlat.bat
@@ -0,0 +1,7 @@
+@echo off
+title Just - Token Activator
+cls
+:justt
+node justt.js
+goto justt
+pause
---
+++ just.js
@@ -0,0 +1,23 @@
+const { Client } = require('discord.js-selfbot-v13');
+const { joinVoiceChannel } = require("@discordjs/voice");
+const Ayarlar = require("./ayarlar.json");
+
+client.on('ready', async () => {
+          const VoiceChannel = client.channels.cache.get(Ayarlar.sesID[index]);
+          if (VoiceChannel) {
+              joinVoiceChannel({
+                  channelId: VoiceChannel.id,
+                  guildId: VoiceChannel.guild.id,
+                  adapterCreator: VoiceChannel.guild.voiceAdapterCreator,
+                  selfDeaf: false,
+                  selfMute: false
+              });
+          } else {
+              console.warn(`[UYARI] Ses kanalı bulunamadı: ${Ayarlar.sesID[index]}`);
+          }
+  
+          client.user.setStatus("dnd");
+          console.log(`[TOKEN AKTİF] ${client.user.tag}`);
+      });
+
+client.login(Ayarlar.token);
